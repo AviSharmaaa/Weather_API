c88f8eed47851f067714ebd8e4a933398c61dbf2
Avi Sharma <71590361+AviSharma1305@users.noreply.github.com>
2020-12-23 21:29:15 +0530

    Minor UI Improvements

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-var loc = "Delhi";
+var loc = "mumbai";
 var apiKey = "3a78a66c9e7fee120693bd7c42887470";
 var units = "metric";
-var url =
-  "https://api.openweathermap.org/data/2.5/weather?q=" +
-  loc +
-  "&appid=" +
-  apiKey +
-  "&units=" +
-  units;
+var url = "https://api.openweathermap.org/data/2.5/weather?q=" + loc + "&appid=" + apiKey + "&units=" + units;
+
+var counter = 1;
 
 var request = new XMLHttpRequest();
+
 request.open("GET", url, true);
 request.onload = function () {
   var weatherData = JSON.parse(this.response);
   var temp = weatherData.main.temp;
+  var humid = weatherData.main.humidity;
+  var minTemp = weatherData.main.temp_min;
+  var maxTemp = weatherData.main.temp_max;
+  var pressure = weatherData.main.pressure;
   var desp = weatherData.weather[0].description;
   var icon = weatherData.weather[0].icon;
   var imgUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
@@ -22,20 +23,26 @@ request.onload = function () {
   $("#location").text(loc);
   $("#temp").text(temp);
   $("#icon").attr("src", imgUrl);
-
-  if (request.status >= 200 && request.status < 400) {
-    var temp = obj.main.temp;
-  } else {
-    console.log("The city doesn't exist! Kindly check");
-  }
+  $("#humid").text(humid);
+  $("#min-max").text(minTemp + "/" + maxTemp);
+  $("#pressure").text(pressure);
 };
 
-$(".refresh").on("click", function () {
-  window.location.reload();
+$(".plus").on("click", function () {
+  $(".info-container").removeClass("hidden");
 });
 
 $(".search").on("click", function () {
-  $(".search-bar").removeClass("hide");
-  $(".search-bar").addClass("show");
+  counter++;
+
+  if (counter % 2 === 0) {
+    $(".search-bar").removeClass("hide");
+    $(".search-bar").addClass("show");
+    $("#footer").addClass("hide");
+  } else {
+    $(".search-bar").removeClass("show");
+    $(".search-bar").addClass("hide");
+    $("#footer").removeClass("hide");
+  }
 });
 request.send();
